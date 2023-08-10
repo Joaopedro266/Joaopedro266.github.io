79fe0b38f0cb5e94388c4c34c59a024198cdb2f9
João Pedro Nunes <92180095+Joaopedro266@users.noreply.github.com>
2023-08-10 09:13:34 -0300

    Add files via upload

---
+++ calcular.js
@@ -0,0 +1,23 @@
+function calcular() {
+   var inputmedia = parseFloat(document.getElementById("media").value);
+   var inputvitorias = parseFloat(document.getElementById("vitorias").value);
+   var resultadinho = document.getElementById("resultado");
+   if(inputmedia > 28) {
+      resultadinho.innerText = "Va fazer algo da vida vagabundo";
+   }
+   else{
+      var mediazinha = inputvitorias / inputmedia;
+         if (mediazinha < 0.5){
+            resultadinho.innerText = "Voce é analfabeto, anta, azêmola, bronco, cavalgadura, estulto, estúpido, idiota, ignorante, imbecil, inepto, lerdaço, néscio, palerma, parvo, tapado, teimoso, tolo, toupeira, armação de serrador";
+         }
+         else {
+            resultadinho.innerText = "Sua média de vitórias é = " + mediazinha.toFixed(1) + " de 1.0 por semana";
+         }
+   }
+
+}
+   
+function botaocampeao(nome) {
+   document.getElementById("resultadoperso").innerText = "Seu resultado de personalidade é = " + nome;
+}
+   
