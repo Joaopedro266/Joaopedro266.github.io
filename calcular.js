c7e9ee4d84ffe21bc96f9ed80292398cd1cdd169
João Pedro Nunes <92180095+Joaopedro266@users.noreply.github.com>
2023-08-10 09:24:59 -0300

    calcular.js

--- calcular.js
+++ calcular.js
@@ -8,7 +8,7 @@ function calcular() {
    else{
       var mediazinha = inputvitorias / inputmedia;
          if (mediazinha < 0.5){
-            resultadinho.innerText = "Voce é analfabeto, anta, azêmola, bronco, cavalgadura, estulto, estúpido, idiota, ignorante, imbecil, inepto, lerdaço, néscio, palerma, parvo, tapado, teimoso, tolo, toupeira, armação de serrador";
+            resultadinho.innerText = "Sua media deu menos que 0.5 logo voce é analfabeto, anta, azêmola, bronco, cavalgadura, estulto, estúpido, idiota, ignorante, imbecil, inepto, lerdaço, néscio, palerma, parvo, tapado, teimoso, tolo, toupeira, armação de serrador";
          }
          else {
             resultadinho.innerText = "Sua média de vitórias é = " + mediazinha.toFixed(1) + " de 1.0 por semana";
